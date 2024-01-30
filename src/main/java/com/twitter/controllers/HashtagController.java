c498b132a47e160feafbfc85e8f4c4ccee9275de
Monkey 0xJungle.com <Monkey@0xJungle.com>
2024-01-30 10:43:55 -0500

    add controllers and mappers

---
+++ src/main/java/com/twitter/controllers/HashtagController.java
@@ -0,0 +1,12 @@
+package com.twitter.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hashtag")
+public class HashtagController {
+
+}
