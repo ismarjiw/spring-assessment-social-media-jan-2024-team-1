b0a178357221eacbb9e857815751290ad85f2291
Monkey 0xJungle.com <Monkey@0xJungle.com>
2024-01-31 19:15:33 -0500

    finished validates endpoints

--- src/main/java/com/twitter/controllers/ValidateController.java
+++ src/main/java/com/twitter/controllers/ValidateController.java
@@ -1,6 +1,10 @@
 package com.twitter.controllers;
 
+import com.twitter.services.HashtagService;
+import com.twitter.services.ValidateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/validate")
 public class ValidateController {
+    private final ValidateService validateService;
+    @GetMapping("/tag/exists/{label}")
+    public boolean checkHashtagExists(@PathVariable String label){
+    return validateService.checkHashtagExists(label);
+    }
+    @GetMapping("/username/exists/@{username}")
+    public boolean checkUserExists(@PathVariable String username){
+        return  validateService.checkUserExists(username);
+    }
+    @GetMapping("/username/available/@{username}")
+    public  boolean checkUsernameAvailable(@PathVariable String username){
+        return  validateService.checkUsernameAvailable(username);
+    }
+
 }
--- src/main/java/com/twitter/services/ValidateService.java
+++ src/main/java/com/twitter/services/ValidateService.java
@@ -3,4 +3,9 @@ package com.twitter.services;
 
 public interface ValidateService {
 
+    boolean checkHashtagExists(String label);
+
+    boolean checkUserExists(String username);
+
+    boolean checkUsernameAvailable(String username);
 }
